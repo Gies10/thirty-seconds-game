4a3cff3432c421981aad24a7c17e4928ecf9d9d9
ghoving_deloitte <ghoving@deloitte.nl>
2026-02-04 11:48:53 +0100

    Include firebase-config.js for GitHub Pages deployment

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+// Firebase Configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBNW0VrWPUoluOAbQDj7lY29M9z6QghePM",
+    authDomain: "thirty-seconds-game-d6677.firebaseapp.com",
+    databaseURL: "https://thirty-seconds-game-d6677-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "thirty-seconds-game-d6677",
+    storageBucket: "thirty-seconds-game-d6677.firebasestorage.app",
+    messagingSenderId: "636172598811",
+    appId: "1:636172598811:web:8a854d2ff710b3b3d542be",
+    measurementId: "G-CC0ZJS4R43"
+};
